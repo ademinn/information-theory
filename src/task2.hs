97c9fe6420ca6857669f50a28ceb0de09391098d
Andrew Demin <ademinn@gmail.com>
2012-11-24 16:40:07 +0400

    parts 1 and 2

--- src/task2.hs
+++ src/task2.hs
@@ -1,14 +1,15 @@
 import System.IO
 import Text.Printf
 import qualified Data.Map as Map
+import qualified Data.List as List
 
-calcFreq :: Int -> String -> Map.Map String Int
-calcFreq n s = if length s' >= n
+calcFreq :: (Integral i) => i -> String -> Map.Map String i
+calcFreq n s = if List.genericLength s' >= n
     then Map.insertWith (+) s' 1 $ calcFreq n $ tail s
     else Map.empty
-    where s' = take n s
+    where s' = List.genericTake n s
 
-calcEntropy :: Int -> Double -> (Int -> Double) -> Map.Map String Int -> Double
+calcEntropy :: (Integral i) => i -> Double -> (i -> Double) -> Map.Map String i -> Double
 calcEntropy k acc calcProb m = (foldr f acc $ Map.elems m) / fromIntegral k
     where f val acc' = acc' - prob * logBase 2.0 prob where prob = calcProb val
 
@@ -18,7 +19,8 @@ printfLn format = printf $ format ++ if nativeNewline == LF then "\n" else "\r\n
 main :: IO ()
 main = do
     s <- hGetContents =<< openFile "BIB" ReadMode
-    let len = length s
+    let len :: Integer
+        len = List.genericLength s
         calcProb k val = (fromIntegral val) / (fromIntegral (len - k) + 1.0)
         calcAns k = (calcEntropy k 0.0 (calcProb k) m, calcEntropy k acc ((*) norm . calcProb k) m)
             where
@@ -30,5 +32,5 @@ main = do
                 acc = (fromIntegral $ cnt - a) * prob * logBase 2.0 prob
         printAns k = uncurry (printfLn "%d %10.4f %10.4f" k) (calcAns k)
     printfLn "%s %10s %10s" "n" "0" "1/(N^n)"
-    sequence $ map printAns [1, 2, 3, 4]
+    sequence $ map printAns [1, 2, 3, 4, 5, 6]
     return ()
